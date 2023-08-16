f079d6ad39bae3534a7f4967c51172aea296993c
pm-julian <131877427+pm-julian@users.noreply.github.com>
2023-08-16 17:34:50 +0200

    Create mainprogramms.sh
    Ubuntu Docker, Postman and GitHub Desktop

---
+++ mainprogramms.sh
@@ -0,0 +1,46 @@
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+
+sudo chmod a+x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo groupadd docker
+
+sudo usermod -aG docker USER
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+
+
+sudo apt update
+
+sudo snap install postman
+
+
+
+sudo apt update
+
+sudo apt install git
+
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/GitHubDesktop-linux-3.1.1-linux1.deb
+
+sudo apt install gdebi-core
+
+sudo gdebi GitHubDesktop-linux-3.1.1-linux1.deb
