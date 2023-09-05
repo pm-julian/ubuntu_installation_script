5309a34e17d12f749cf8ccd410b7bd8810b78482
pm-julian <131877427+pm-julian@users.noreply.github.com>
2023-09-05 11:31:47 +0200

    Update mainprogramms.sh
    Ja

--- mainprogramms.sh
+++ mainprogramms.sh
@@ -42,7 +42,7 @@ sudo snap install --classic code
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
