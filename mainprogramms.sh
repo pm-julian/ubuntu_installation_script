27bab093af5ac7e0bf4275481db9148b2a66baaf
pm-julian <131877427+pm-julian@users.noreply.github.com>
2023-09-05 10:07:21 +0200

    Update mainprogramms.sh
    Svens Spielwiese

--- mainprogramms.sh
+++ mainprogramms.sh
@@ -13,8 +13,6 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
@@ -34,14 +32,10 @@ sudo systemctl enable containerd.service
 
 
 # Postman
-sudo apt update
-
 sudo snap install postman
 
 
 # Git Hub Desktop
-sudo apt update
-
 sudo apt install -y git
 
 sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/GitHubDesktop-linux-3.1.1-linux1.deb
@@ -62,6 +56,18 @@ curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install
 
 #OpenVPN
 sudo apt-get install openvpn
+
+#Slack
+sudo snap install slack --classic
+
+#Flameshot
+sudo apt install flameshot
+
+#ZSH
+sudo apt install zsh -y
+
+#VSCode
+sudo snap install --classic code
 
 
 
