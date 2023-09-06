5adf78eec69f800c04e0de3a9abd072f3375de3b
pm-julian <131877427+pm-julian@users.noreply.github.com>
2023-09-06 17:04:33 +0200

    Update mainprogramms.sh
    Anlegen des Root-Users

--- mainprogramms.sh
+++ mainprogramms.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# Anlegen des Root-Users
+# Überprüfen, ob das Skript als Root ausgeführt wird
+if [ "$EUID" -eq 0 ]; then
+    echo "Dieses Skript sollte nicht als Root-Benutzer ausgeführt werden."
+    exit 1
+fi
+
+# Benutzernamen festlegen
+USERNAME="pm-root"
+
+# Passwort vom Benutzer abfragen
+read -s -p "Geben Sie das Passwort für den Benutzer $USERNAME ein: " PASSWORD
+echo
+
+# Benutzer erstellen und Shell-Zuweisung hinzufügen
+sudo useradd -m -s /bin/bash $USERNAME
+
+# Passwort für den Benutzer festlegen
+echo "$USERNAME:$PASSWORD" | sudo chpasswd
+
+# Benutzer zur sudo-Gruppe hinzufügen
+sudo usermod -aG sudo $USERNAME
+
+echo "Der Benutzer $USERNAME wurde erstellt und zur sudo-Gruppe hinzugefügt."
+
+
+
+# Install Beginn
 sudo apt-get update
 
 # Postman
@@ -18,22 +46,22 @@ sudo gdebi GitHubDesktop-linux-3.1.1-linux1.deb -n
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-#OpenVPN
+# OpenVPN
 sudo apt-get install openvpn -y
 
-#Slack
+# Slack
 sudo snap install slack --classic
 
-#Flameshot
+# Flameshot
 sudo apt install flameshot -y
 
-#ZSH
+# ZSH
 sudo apt install zsh -y
 
-#VSCode
+# VSCode
 sudo snap install --classic code
 
-#Jetbrains toolbox
+# Jetbrains toolbox
 sudo apt install libfuse2
 curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
 
